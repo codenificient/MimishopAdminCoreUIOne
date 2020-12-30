079c5d6cba0dbdbf5bbec5ebb91ab79c6f89fe80
CodenificenT <61930093+siechristian@users.noreply.github.com>
2020-12-30 18:14:55 -0500

    french up checkout page

--- mimishop/src/components/Header/index.js
+++ mimishop/src/components/Header/index.js
@@ -74,22 +74,22 @@ const Header = (props) => {
 		return (
 			<DropdownMenu
 				menu={
-					<a className="loginButton" onClick={() => setLoginModal(true)}>
-						Login
+					<a className="loginButton" style={{ color: '#177c7c' }} onClick={() => setLoginModal(true)}>
+						Connection
 					</a>
 				}
 				menus={[
-					{ label: 'My Profile', href: '', icon: null },
-					{ label: 'Flipkart Plus Zone', href: '', icon: null },
-					{ label: 'Orders', href: '', icon: null },
-					{ label: 'Wishlist', href: '', icon: null },
-					{ label: 'Rewards', href: '', icon: null },
-					{ label: 'Gift Cards', href: '', icon: null }
+					{ label: 'Mon Profile', href: '', icon: null },
+					{ label: 'Commandes', href: '', icon: null },
+					{ label: 'Zone VIP', href: '', icon: null },
+					{ label: 'Liste de Souhaits', href: '', icon: null },
+					{ label: 'Récompenses', href: '', icon: null },
+					{ label: 'Bons et Cadeaux', href: '', icon: null }
 				]}
 				firstMenu={
 					<div className="firstmenu">
-						<span>New Customer?</span>
-						<a style={{ color: '#2874f0' }}>Sign Up</a>
+						<span style={{ color: '#177c7c', fontSize: '14px' }}>Nouveau Client?</span>
+						<a style={{ color: '#177c7c', fontSize: '14px', fontWeight: '600' }}>Créer un compte</a>
 					</div>
 				}
 			/>
@@ -188,11 +188,11 @@ const Header = (props) => {
 							</a>
 						}
 						menus={[
-							{ label: 'Notification Preference', href: '', icon: null },
-							{ label: 'Sell on flipkart', href: '', icon: null },
-							{ label: '24x7 Customer Care', href: '', icon: null },
-							{ label: 'Advertise', href: '', icon: null },
-							{ label: 'Download App', href: '', icon: null }
+							{ label: 'Préferences de Notifications', href: '', icon: null },
+							{ label: 'Vendre sur MimiShop', href: '', icon: null },
+							{ label: 'Service Client 24x7', href: '', icon: null },
+							{ label: 'Annonces et Publicités', href: '', icon: null },
+							{ label: 'Application MimiShop', href: '', icon: null }
 						]}
 					/>
 					<div>
