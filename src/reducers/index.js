9d94d1c749db9a0eb9e0abc3decd58a46d56e7c6
CodenificenT <61930093+siechristian@users.noreply.github.com>
2021-02-19 15:57:08 -0500

    add current state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,21 +1,20 @@
-import React from 'react'
-import { combineReducers } from 'redux'
+import { combineReducers } from 'redux';
 
 const initialState = {
-  sidebarShow: 'responsive'
-}
+	sidebarShow: 'responsive'
+};
 
 const changeState = (state = initialState, { type, ...rest }) => {
-  switch (type) {
-    case 'set':
-      return {...state, ...rest }
-    default:
-      return state
-  }
-}
+	switch (type) {
+		case 'set':
+			return { ...state, ...rest };
+		default:
+			return state;
+	}
+};
 
 const rootReducer = combineReducers({
-    nav: changeState,
-})
+	nav: changeState
+});
 
-export default rootReducer
+export default rootReducer;
